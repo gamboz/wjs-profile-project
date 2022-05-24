e3abb036ad98c2e147751f62c9f3df51cdeef42d
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-05-24 16:58:09 +0200

    Add registration url, view and form for step 1.
    Seems to work...

--- wjs_jcom_profile/forms.py
+++ wjs_jcom_profile/forms.py
@@ -1,6 +1,11 @@
 """Forms for the additional fields in this profile extension."""
 
+import uuid
+from django import forms
 from django.forms import ModelForm
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
+from utils.forms import CaptchaForm
 from wjs_jcom_profile.models import JCOMProfile
 
 
@@ -10,3 +15,46 @@ class JCOMProfileForm(ModelForm):
     class Meta:
         model = JCOMProfile
         fields = '__all__'
+
+
+class JCOMRegistrationForm(ModelForm, CaptchaForm):
+    """A form that creates a user.
+
+    With no privileges, from the given username and password.
+
+    """
+
+    password_1 = forms.CharField(
+        widget=forms.PasswordInput, label=_('Password'))
+    password_2 = forms.CharField(
+        widget=forms.PasswordInput, label=_('Repeat Password'))
+
+    class Meta:
+        model = JCOMProfile
+        fields = ('email', 'salutation', 'first_name', 'middle_name',
+                  'last_name', 'department', 'institution', 'country',
+                  'profession')
+
+    def clean_password_2(self):
+        """Validate password."""
+        password_1 = self.cleaned_data.get("password_1")
+        password_2 = self.cleaned_data.get("password_2")
+        if password_1 and password_2 and password_1 != password_2:
+            raise forms.ValidationError(
+                'Your passwords do not match.',
+                code='password_mismatch',
+            )
+        return password_2
+
+    def save(self, commit=True):
+        """Save all (parent and me)."""
+        user = super(JCOMRegistrationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password_1"])
+        user.is_active = False
+        user.confirmation_code = uuid.uuid4()
+        user.email_sent = timezone.now()
+
+        if commit:
+            user.save()
+
+        return user
--- wjs_jcom_profile/urls.py
+++ wjs_jcom_profile/urls.py
@@ -6,5 +6,8 @@ from plugins.wjs_jcom_profile import views
 
 urlpatterns = [
     url(r'^profile/$', views.prova, name='core_edit_profile'),
-    # url(r'^prova/$', views.prova, name='core_edit_profile'),
+    url(r'^register/step/1/$', views.register, name='core_register'),
+    # url(r'^register/step/2/(?P<token>[\w-]+)/$', core_views.activate_account, name='core_confirm_account'),
+    # url(r'^register/step/orcid/(?P<token>[\w-]+)/$', core_views.orcid_registration, name='core_orcid_registration'),
+
 ]
