90da70aa47278e351962461e9a06c9f753c0782b
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-05-30 15:40:03 +0200

    tidy up based on BirkbeckCTP/generic_plugin_generator

--- wjs_jcom_profile/plugin_settings.py
+++ wjs_jcom_profile/plugin_settings.py
@@ -1,34 +1,37 @@
-"""Default configuration for Janeway's plugin."""
+"""JCOM profile - Janeway's plugin configuration."""
 
-from utils import models
+from utils import plugins
 
 PLUGIN_NAME = 'JCOM user profile'
-DESCRIPTION = """Enrich Janeway's user profile with a field "profession"."""
-AUTHOR = 'mg'
-VERSION = '1.0'
+DISPLAY_NAME = 'JCOM profile'
+DESCRIPTION = 'Enrich default Janeway account with fields related to JCOM'
+AUTHOR = 'Matteo'
+VERSION = '0.1'
 SHORT_NAME = 'wjs_jcom_profile'
-# MANAGER_URL = 'books_admin'
+MANAGER_URL = 'wjs_jcom_profile_manager'
 JANEWAY_VERSION = "1.4"
 
 
-def install():
-    """TODO: what should I do here?."""
-    new_plugin, created = models.Plugin.objects.get_or_create(
-        name=SHORT_NAME,
-        enabled=True,
-        # This should be "False", because the enriched profile is
-        # useful only for JCOM, but the DB tables and URL routing are
-        # general. So I'm keeping it "True".
-        press_wide=True,
-        defaults={'version': VERSION},
-    )
+class Wjs_jcom_profilePlugin(plugins.Plugin):
+    plugin_name = PLUGIN_NAME
+    display_name = DISPLAY_NAME
+    description = DESCRIPTION
+    author = AUTHOR
+    short_name = SHORT_NAME
+    manager_url = MANAGER_URL
 
-    if created:
-        print('Plugin {0} installed.'.format(PLUGIN_NAME))
-    else:
-        print('Plugin {0} is already installed.'.format(PLUGIN_NAME))
+    version = VERSION
+    janeway_version = JANEWAY_VERSION
+    
+
+
+def install():
+    Wjs_jcom_profilePlugin.install()
 
 
 def hook_registry():
-    """TODO: what should I do here?."""
+    Wjs_jcom_profilePlugin.hook_registry()
+
+
+def register_for_events():
     pass
