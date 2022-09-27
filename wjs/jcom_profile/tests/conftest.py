f6b6e869d250b1245366f889a19151ba2c742837
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-09-27 16:26:36 +0200

    Reset more messy code...

--- wjs/jcom_profile/tests/conftest.py
+++ wjs/jcom_profile/tests/conftest.py
@@ -137,25 +137,14 @@ def journal(press):
     journal.delete()
 
 
-# You can replace the ordinary django_db_setup to completely avoid
-# database creation/migrations. If you have no need for rollbacks or
-# truncating tables, you can simply avoid blocking the database and
-# use it directly. When using this method you must ensure that your
-# tests do not change the database state.
-#
-# https://pytest-django.readthedocs.io/en/latest/database.html
-#
-#
-# @pytest.fixture(scope='session')
-# def django_db_setup():
-#     """Avoid creating/setting up the test database."""
-#     pass
-#
-#
-# @pytest.fixture
-# def db_access_without_rollback_and_truncate(
-#         request,
-#         django_db_setup, django_db_blocker):
-#     """Do not clean the DB."""
-#     django_db_blocker.unblock()
-#     request.addfinalizer(django_db_blocker.restore)
+@pytest.fixture
+def clear_script_prefix_fix():
+    """Clear django's script prefix at the end of the test.
+
+    Otherwise `reverse()` might produce unexpected results.
+
+    This fixture clears the script prefix before and after the test.
+    """
+    clear_script_prefix()
+    yield None
+    clear_script_prefix()
