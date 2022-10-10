4caf6e309719ff219dc9f88944b4a3c712f6076a
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-10-10 19:16:26 +0200

    POC: intercept URLs via urls module

--- wjs/jcom_profile/views.py
+++ wjs/jcom_profile/views.py
@@ -1,21 +1,16 @@
 """My views. Looking for a way to "enrich" Janeway's `edit_profile`."""
-import base64
-import json
-
-from django.urls import reverse
-from django.conf import settings
-from django.contrib import messages
-from django.db import IntegrityError
-from django.shortcuts import render, redirect
-from django.shortcuts import get_object_or_404
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
-from django.contrib.auth.decorators import login_required
-from django.core.mail import send_mail
-
 from core import logic
 from core import models as core_models
-
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ValidationError
+from django.core.mail import send_mail
+from django.core.validators import validate_email
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from django.views.generic import TemplateView
 from wjs.jcom_profile import forms
 from wjs.jcom_profile.forms import JCOMProfileForm, JCOMRegistrationForm
 from wjs.jcom_profile.models import JCOMProfile
@@ -30,68 +25,75 @@ def prova(request):
 
     # from core.views.py::edit_profile:358ss
     if request.POST:
-        if 'email' in request.POST:
-            email_address = request.POST.get('email_address')
+        if "email" in request.POST:
+            email_address = request.POST.get("email_address")
             try:
                 validate_email(email_address)
                 try:
                     logic.handle_email_change(request, email_address)
-                    return redirect(reverse('website_index'))
+                    return redirect(reverse("website_index"))
                 except IntegrityError:
                     messages.add_message(
                         request,
                         messages.WARNING,
-                        'An account with that email address already exists.',
+                        "An account with that email address already exists.",
                     )
             except ValidationError:
                 messages.add_message(
                     request,
                     messages.WARNING,
-                    'Email address is not valid.',
+                    "Email address is not valid.",
                 )
 
-        elif 'change_password' in request.POST:
-            old_password = request.POST.get('current_password')
-            new_pass_one = request.POST.get('new_password_one')
-            new_pass_two = request.POST.get('new_password_two')
+        elif "change_password" in request.POST:
+            old_password = request.POST.get("current_password")
+            new_pass_one = request.POST.get("new_password_one")
+            new_pass_two = request.POST.get("new_password_two")
 
             if old_password and request.user.check_password(old_password):
 
                 if new_pass_one == new_pass_two:
-                    problems = request.user.password_policy_check(request,
-                                                                  new_pass_one)
+                    problems = request.user.password_policy_check(
+                        request, new_pass_one
+                    )
                     if not problems:
                         request.user.set_password(new_pass_one)
                         request.user.save()
-                        messages.add_message(request, messages.SUCCESS,
-                                             'Password updated.')
+                        messages.add_message(
+                            request, messages.SUCCESS, "Password updated."
+                        )
                     else:
-                        [messages.add_message(
-                            request,
-                            messages.INFO,
-                            problem) for problem in problems]
+                        [
+                            messages.add_message(
+                                request, messages.INFO, problem
+                            )
+                            for problem in problems
+                        ]
                 else:
-                    messages.add_message(request, messages.WARNING,
-                                         'Passwords do not match')
+                    messages.add_message(
+                        request, messages.WARNING, "Passwords do not match"
+                    )
 
             else:
-                messages.add_message(request, messages.WARNING,
-                                     'Old password is not correct.')
+                messages.add_message(
+                    request, messages.WARNING, "Old password is not correct."
+                )
 
-        elif 'edit_profile' in request.POST:
+        elif "edit_profile" in request.POST:
             form = JCOMProfileForm(request.POST, request.FILES, instance=user)
 
             if form.is_valid():
                 form.save()
-                messages.add_message(request, messages.SUCCESS,
-                                     'Profile updated.')
-                return redirect(reverse('core_edit_profile'))
+                messages.add_message(
+                    request, messages.SUCCESS, "Profile updated."
+                )
+                return redirect(reverse("core_edit_profile"))
 
-        elif 'export' in request.POST:
+        elif "export" in request.POST:
             return logic.export_gdpr_user_profile(user)
 
     context = dict(form=form, user_to_edit=user)
-    template = 'core/accounts/edit_profile.html'
+    template = "core/accounts/edit_profile.html"
     return render(request, template, context)
 
 
@@ -105,7 +107,7 @@ def register(request):
     :param request: HttpRequest object
     :return: HttpResponse object
     """
-    token, token_obj = request.GET.get('token', None), None
+    token, token_obj = request.GET.get("token", None), None
     if token:
         token_obj = get_object_or_404(core_models.OrcidToken, token=token)
 
@@ -118,7 +120,7 @@ def register(request):
 
         if password_policy_check:
             for policy_fail in password_policy_check:
-                form.add_error('password_1', policy_fail)
+                form.add_error("password_1", policy_fail)
 
         if form.is_valid():
             if token_obj:
@@ -130,27 +132,28 @@ def register(request):
                 new_user = form.save()
 
             if request.journal:
-                new_user.add_account_role('author', request.journal)
+                new_user.add_account_role("author", request.journal)
             logic.send_confirmation_link(request, new_user)
 
             messages.add_message(
                 request,
                 messages.SUCCESS,
-                'Your account has been created, please follow the'
-                'instructions in the email that has been sent to you.')
-            return redirect(reverse('core_login'))
+                "Your account has been created, please follow the"
+                "instructions in the email that has been sent to you.",
+            )
+            return redirect(reverse("core_login"))
 
-    template = 'core/accounts/register.html'
+    template = "core/accounts/register.html"
     context = {
-        'form': form,
+        "form": form,
     }
 
     return render(request, template, context)
 
 
 def confirm_gdpr_acceptance(request, token):
-    """
-    The view for explicit GDPR acceptance for invited users.
+    """Explicitly confirm GDPR acceptance for invited users.
+
     The token encodes base user information (name, surname and email)
     """
     template = "admin/core/account/gdpr_acceptance.html"
@@ -179,20 +182,77 @@ def confirm_gdpr_acceptance(request, token):
                 account.save()
                 context["activated"] = True
                 # Generate a temporary token to set a brand-new password
-                core_models.PasswordResetToken.objects.filter(account=account).update(expired=True)
-                reset_token = core_models.PasswordResetToken.objects.create(account=account)
+                core_models.PasswordResetToken.objects.filter(
+                    account=account
+                ).update(expired=True)
+                reset_token = core_models.PasswordResetToken.objects.create(
+                    account=account
+                )
                 reset_psw_url = request.build_absolute_uri(
-                    reverse("core_reset_password", kwargs={"token": reset_token.token}))
+                    reverse(
+                        "core_reset_password",
+                        kwargs={"token": reset_token.token},
+                    )
+                )
                 # Send email.
                 # FIXME: Email setting should be handled using the janeway settings framework.
                 # See https://gitlab.sissamedialab.it/wjs/wjs-profile-project/-/issues/4
                 send_mail(
                     settings.RESET_PASSWORD_SUBJECT,
-                    settings.RESET_PASSWORD_BODY.format(account.first_name, account.last_name, reset_psw_url),
+                    settings.RESET_PASSWORD_BODY.format(
+                        account.first_name, account.last_name, reset_psw_url
+                    ),
                     settings.DEFAULT_FROM_EMAIL,
-                    [account.email]
+                    [account.email],
                 )
         else:
             context["form"] = form
 
     return render(request, template, context)
+
+
+from django import forms
+from utils.logger import get_logger
+logger = get_logger(__name__)
+
+class TMPForm(forms.Form):
+    dummy = forms.CharField(max_length=11)
+
+
+class SpecialIssues(TemplateView):
+    """Views related to a journal's special issues."""
+
+    form_class = TMPForm
+    template_name = "admin/submission/submit_si_chooser.html"
+    # success_url = reverse("submit_infozero",
+    #                       kwargs={"article_id": ???)
+
+    def post(self, *args, **kwargs):
+        """Boh."""
+        logger.debug("CALLED POST")
+        form = self.form_class(self.request.POST)
+        if form.is_valid():
+            # import ipdb; ipdb.set_trace()
+            return redirect(
+                reverse(
+                    "submit_infozero",
+                    kwargs={"article_id": kwargs["article_id"]},
+                )
+            )
+        context = dict(form=form)
+        return render(
+            self.request,
+            template_name=self.template_name,
+            context=context,
+        )
+
+    def get(self, *args, **kwargs):
+        """Boh."""
+        logger.debug("CALLED GET")
+        form = self.form_class()
+        context = dict(form=form)
+        return render(
+            self.request,
+            template_name=self.template_name,
+            context=context,
+        )
