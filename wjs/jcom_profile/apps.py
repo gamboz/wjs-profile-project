a80f55f497adf40b93a598e11be16a4b444f84c0
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-10-10 15:27:34 +0200

    Move registration of our functionto J. events in apps module

--- wjs/jcom_profile/apps.py
+++ wjs/jcom_profile/apps.py
@@ -1,33 +1,42 @@
 """Configure this application."""
 # https://docs.djangoproject.com/en/4.0/ref/applications/
 from django.apps import AppConfig
-# import logging
+
+from events import logic as events_logic
+
+from .events.wjs_events import notify_coauthors_article_submission
 
 
 class JCOMProfileConfig(AppConfig):
     """Configuration for this django app."""
 
     name = "wjs.jcom_profile"
-    verbose_name = 'WJS JCOM profile'
+    verbose_name = "WJS JCOM profile"
 
     def ready(self):
         """Call during initialization."""
-        # import ipdb; ipdb.set_trace()
-        from wjs.jcom_profile import signals
-        from wjs.jcom_profile import urls
+        from wjs.jcom_profile import signals, urls
 
-        # logging.warning("✨CALLED")
         self.register_hooks()
+
+        events_logic.Events.register_for_event(
+            events_logic.Events.ON_ARTICLE_SUBMITTED,
+            notify_coauthors_article_submission,
+        )
 
     def register_hooks(self):
         """Register my functions to Janeway's hooks."""
         hooks = [
             # dict(nav_block=dict(module='wjs.jcom_profile.hooks',
             #                     function='prova_hook')),
-            dict(extra_corefields=dict(module='wjs.jcom_profile.hooks',
-                                       function='prova_hook')),
+            dict(
+                extra_corefields=dict(
+                    module="wjs.jcom_profile.hooks", function="prova_hook"
+                )
+            ),
         ]
         # NB: do not `import core...` before `ready()`,
         # otherwise django setup process breaks
         from core import plugin_loader
+
         plugin_loader.register_hooks(hooks)
