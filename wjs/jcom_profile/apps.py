2e050367cbf3e7091cab3d7e26ff243d402cf2f0
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-10-10 16:01:36 +0200

    Delete unused file and wrap registration code in function

--- wjs/jcom_profile/apps.py
+++ wjs/jcom_profile/apps.py
@@ -2,10 +2,6 @@
 # https://docs.djangoproject.com/en/4.0/ref/applications/
 from django.apps import AppConfig
 
-from events import logic as events_logic
-
-from .events.wjs_events import notify_coauthors_article_submission
-
 
 class JCOMProfileConfig(AppConfig):
     """Configuration for this django app."""
@@ -18,11 +14,7 @@ class JCOMProfileConfig(AppConfig):
         from wjs.jcom_profile import signals, urls
 
         self.register_hooks()
-
-        events_logic.Events.register_for_event(
-            events_logic.Events.ON_ARTICLE_SUBMITTED,
-            notify_coauthors_article_submission,
-        )
+        self.register_events()
 
     def register_hooks(self):
         """Register my functions to Janeway's hooks."""
@@ -40,3 +32,15 @@ class JCOMProfileConfig(AppConfig):
         from core import plugin_loader
 
         plugin_loader.register_hooks(hooks)
+
+    def register_events(self):
+        """Register our function in Janeway's events logic."""
+        from wjs.jcom_profile.events.wjs_events import \
+            notify_coauthors_article_submission
+
+        from events import logic as events_logic
+
+        events_logic.Events.register_for_event(
+            events_logic.Events.ON_ARTICLE_SUBMITTED,
+            notify_coauthors_article_submission,
+        )
