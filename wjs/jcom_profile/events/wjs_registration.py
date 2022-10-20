8547da2dacfe3cddd67247241899f1b920ec3ce3
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2022-10-20 08:43:58 +0200

    Remove uselesse wjs_registration.py file

--- wjs/jcom_profile/events/wjs_registration.py
+++
@@ -1,7 +0,0 @@
-"""Register all the custom events hooks using Janeway events framework."""
-
-from events import logic as events_logic
-
-from wjs.jcom_profile.events.wjs_events import notify_coauthors_article_submission
-
-events_logic.Events.register_for_event(events_logic.Events.ON_ARTICLE_SUBMITTED, notify_coauthors_article_submission)
