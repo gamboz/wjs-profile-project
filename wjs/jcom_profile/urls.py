a7e9cf51006190e05e7578125b200e546d9a3631
Matteo Gamboz <gamboz@medialab.sissa.it>
2022-11-14 16:15:33 +0100

    Layout "new" and "update" templates in app-style
    and more tidy-up

--- wjs/jcom_profile/urls.py
+++ wjs/jcom_profile/urls.py
@@ -31,9 +31,15 @@ urlpatterns = [
         name="submit_info_original",
     ),
     # Special Issues mgmt
-    url(r"^si/new$", views.SICreate.as_view(template_name="si_new.html"), name="si-create"),
+    #     add, view, update
+    url(r"^manage/si/new$", views.SICreate.as_view(template_name="admin/core/si_new.html"), name="si-create"),
     url(r"^si/(?P<pk>\d+)/$", views.SIDetails.as_view(template_name="si_details.html"), name="si-details"),
-    url(r"^si/(?P<pk>\d+)/edit$", views.SIUpdate.as_view(template_name="si_update.html"), name="si-update"),
+    url(
+        r"^manage/si/(?P<pk>\d+)/edit$",
+        views.SIUpdate.as_view(template_name="admin/core/si_update.html"),
+        name="si-update",
+    ),
+    #     files (aka documents; upload & download)
     url(
         r"^si/(?P<special_issue_id>\d+)/file/(?P<file_id>\d+)/",
         views.serve_special_issue_file,
