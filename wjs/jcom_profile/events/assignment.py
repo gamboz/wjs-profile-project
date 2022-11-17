86daf7357fd966efda2e4be7e65ea5811a21ba7b
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2022-11-17 12:45:11 +0100

    Try to fix setting import to fix pipeline

--- wjs/jcom_profile/events/assignment.py
+++ wjs/jcom_profile/events/assignment.py
@@ -2,7 +2,7 @@
 
 Journal level configuration is made using the 'WJS_ARTICLE_ASSIGNMENT_FUNCTIONS' setting
 """
-from core.settings import WJS_ARTICLE_ASSIGNMENT_FUNCTIONS  # NOQA
+from django.conf import settings  # NOQA
 from django.utils.module_loading import import_string
 
 
@@ -14,7 +14,7 @@ def default_assign_editors_to_articles(**kwargs) -> None:
 def dispatch_assignment(**kwargs) -> None:
     """Dispatch editors assignment on journal basis, selecting the requested assignment algorithm."""
     journal = kwargs["article"].journal_id
-    if journal in WJS_ARTICLE_ASSIGNMENT_FUNCTIONS:
-        import_string(WJS_ARTICLE_ASSIGNMENT_FUNCTIONS.get(journal))(**kwargs)
+    if journal in settings.WJS_ARTICLE_ASSIGNMENT_FUNCTIONS:
+        import_string(settings.WJS_ARTICLE_ASSIGNMENT_FUNCTIONS.get(journal))(**kwargs)
     else:
-        import_string(WJS_ARTICLE_ASSIGNMENT_FUNCTIONS.get(None))(**kwargs)
+        import_string(settings.WJS_ARTICLE_ASSIGNMENT_FUNCTIONS.get(None))(**kwargs)
