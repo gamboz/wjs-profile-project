34da72023bdc641423e0a9c980c367982bb11d8b
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2022-12-12 12:55:43 +0100

    Rework test_automatic_article_assignment.py

--- wjs/jcom_profile/tests/test_automatic_article_assignment.py
+++ wjs/jcom_profile/tests/test_automatic_article_assignment.py
@@ -8,87 +8,12 @@ from review.models import EditorAssignment
 
 from wjs.jcom_profile.models import EditorAssignmentParameters
 
-
-@pytest.mark.django_db
-def test_jcom_normal_issue_articles_are_assigned_to_journal_director_with_lowest_workload(
-    admin,
-    article,
-    directors,
-    coauthors_setting,
-):
-    client = Client()
-    client.force_login(admin)
-    lowest_workload = 1000
-    selected_director = None
-    for director in directors:
-        params = EditorAssignmentParameters.objects.create(
-            editor=director,
-            journal=article.journal,
-            workload=random.randint(1, 10),
-        )
-        if params.workload < lowest_workload:
-            selected_director = params.editor
-            lowest_workload = params.workload
-
-    url = reverse("submit_review", args=(article.pk,))
-
-    response = client.post(url, data={"next_step": "next_step"})
-    assert response.status_code == 302
-
-    editor_assignment = EditorAssignment.objects.get(article=article)
-    article.refresh_from_db()
-
-    assert editor_assignment.editor == selected_director
-    assert article.stage == "Assigned"
+WJS_ARTICLE_ASSIGNMENT_FUNCTIONS = {
+    None: "wjs.jcom_profile.events.assignment.default_assign_editors_to_articles",
+}
 
 
-@pytest.mark.django_db
-def test_jcom_special_issue_articles_are_assigned_to_editor_with_lowest_workload_among_special_issue_editors(
-    admin,
-    article,
-    coauthors_setting,
-    special_issue,
-):
-    client = Client()
-    client.force_login(admin)
-    lowest_workload = 1000
-    selected_editor = None
-    for editor in special_issue.editors.all():
-        params = EditorAssignmentParameters.objects.create(
-            editor=editor,
-            journal=special_issue.journal,
-            workload=random.randint(1, 10),
-        )
-        if params.workload < lowest_workload:
-            selected_editor = params.editor
-            lowest_workload = params.workload
-
-    url = reverse("submit_review", args=(article.pk,))
-
-    response = client.post(url, data={"next_step": "next_step"})
-    assert response.status_code == 302
-
-    editor_assignment = EditorAssignment.objects.get(article=article)
-    article.refresh_from_db()
-
-    assert editor_assignment.editor == selected_editor
-    assert article.stage == "Assigned"
-
-
-@pytest.mark.django_db
-@override_settings(
-    WJS_ARTICLE_ASSIGNMENT_FUNCTIONS={
-        None: "wjs.jcom_profile.events.assignment.default_assign_editors_to_articles",
-    },
-)
-def test_default_normal_issue_articles_are_assigned_to_journal_director_with_lowest_workload(
-    admin,
-    article,
-    editors,
-    coauthors_setting,
-):
-    client = Client()
-    client.force_login(admin)
+def get_expected_editor(editors, article):
     lowest_workload = 1000
     selected_editor = None
     for editor in editors:
@@ -100,52 +25,76 @@ def test_default_normal_issue_articles_are_assigned_to_journal_director_with_low
         if params.workload < lowest_workload:
             selected_editor = params.editor
             lowest_workload = params.workload
-
-    url = reverse("submit_review", args=(article.pk,))
-
-    response = client.post(url, data={"next_step": "next_step"})
-    assert response.status_code == 302
-
-    editor_assignment = EditorAssignment.objects.get(article=article)
-    article.refresh_from_db()
-
-    assert editor_assignment.editor == selected_editor
-    assert article.stage == "Assigned"
+    return selected_editor
 
 
-@pytest.mark.django_db
-@override_settings(
-    WJS_ARTICLE_ASSIGNMENT_FUNCTIONS={
-        None: "wjs.jcom_profile.events.assignment.default_assign_editors_to_articles",
-    },
+@pytest.mark.parametrize(
+    "journal_assignment_algorithm_exists",
+    (
+        True,
+        False,
+    ),
 )
-def test_default_special_issue_articles_are_assigned_to_editor_with_lowest_workload_among_special_issue_editors(
+@pytest.mark.django_db
+def test_normal_issue_articles_automatic_assignment(
+    admin,
+    article,
+    directors,
+    coauthors_setting,
+    journal_assignment_algorithm_exists,
+):
+    if journal_assignment_algorithm_exists:
+        WJS_ARTICLE_ASSIGNMENT_FUNCTIONS[
+            article.journal
+        ] = "wjs.jcom_profile.events.assignment.jcom_assign_editors_to_articles"
+    with override_settings(WJS_ARTICLE_ASSIGNMENT_FUNCTIONS=WJS_ARTICLE_ASSIGNMENT_FUNCTIONS):
+        client = Client()
+        client.force_login(admin)
+        selected_director = get_expected_editor(directors, article)
+
+        url = reverse("submit_review", args=(article.pk,))
+
+        response = client.post(url, data={"next_step": "next_step"})
+        assert response.status_code == 302
+
+        editor_assignment = EditorAssignment.objects.get(article=article)
+        article.refresh_from_db()
+
+        assert editor_assignment.editor == selected_director
+        assert article.stage == "Assigned"
+
+
+@pytest.mark.parametrize(
+    "journal_assignment_algorithm_exists",
+    (
+        True,
+        False,
+    ),
+)
+@pytest.mark.django_db
+def test_special_issue_articles_automatic_assignment(
     admin,
     article,
     coauthors_setting,
     special_issue,
+    journal_assignment_algorithm_exists,
 ):
-    client = Client()
-    client.force_login(admin)
-    lowest_workload = 1000
-    selected_editor = None
-    for editor in special_issue.editors.all():
-        params = EditorAssignmentParameters.objects.create(
-            editor=editor,
-            journal=special_issue.journal,
-            workload=random.randint(1, 10),
-        )
-        if params.workload < lowest_workload:
-            selected_editor = params.editor
-            lowest_workload = params.workload
+    if journal_assignment_algorithm_exists:
+        WJS_ARTICLE_ASSIGNMENT_FUNCTIONS[
+            special_issue.journal.code
+        ] = "wjs.jcom_profile.events.assignment.jcom_assign_editors_to_articles"
+    with override_settings(WJS_ARTICLE_ASSIGNMENT_FUNCTIONS=WJS_ARTICLE_ASSIGNMENT_FUNCTIONS):
+        client = Client()
+        client.force_login(admin)
+        selected_editor = get_expected_editor(special_issue.editors.all(), article)
 
-    url = reverse("submit_review", args=(article.pk,))
+        url = reverse("submit_review", args=(article.pk,))
 
-    response = client.post(url, data={"next_step": "next_step"})
-    assert response.status_code == 302
+        response = client.post(url, data={"next_step": "next_step"})
+        assert response.status_code == 302
 
-    editor_assignment = EditorAssignment.objects.get(article=article)
-    article.refresh_from_db()
+        editor_assignment = EditorAssignment.objects.get(article=article)
+        article.refresh_from_db()
 
-    assert editor_assignment.editor == selected_editor
-    assert article.stage == "Assigned"
+        assert editor_assignment.editor == selected_editor
+        assert article.stage == "Assigned"
