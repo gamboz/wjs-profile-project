b6ed815901e4ee266c056f1b088e223197e2ef37
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2022-12-12 09:58:22 +0100

    Add JCOM automatic assignment algorithm

--- wjs/jcom_profile/events/assignment.py
+++ wjs/jcom_profile/events/assignment.py
@@ -15,6 +15,7 @@ def default_assign_editors_to_articles(**kwargs) -> None:
     article = kwargs["article"]
     if article.articlewrapper.special_issue and article.articlewrapper.special_issue.editors:
         parameters = EditorAssignmentParameters.objects.filter(
+            journal=article.journal,
             editor__in=article.articlewrapper.special_issue.editors.all(),
         )
     else:
@@ -25,13 +26,41 @@ def default_assign_editors_to_articles(**kwargs) -> None:
         article.stage = "Assigned"
         article.save()
     else:
-        # TODO: Check out how to deal with this
+        print("No editor parameters.")
+
+
+def jcom_assign_editors_to_articles(**kwargs):
+    """Assign editors to article for review. JCOM algorithm."""
+    from core.models import AccountRole, Role
+    from review.models import EditorAssignment
+
+    from ..models import EditorAssignmentParameters
+
+    article = kwargs["article"]
+    directors = AccountRole.objects.filter(
+        journal=article.journal,
+        role=Role.objects.get(slug="director"),
+    ).values_list("user")
+    parameters = None
+    if article.articlewrapper.special_issue and article.articlewrapper.special_issue.editors:
+        parameters = EditorAssignmentParameters.objects.filter(
+            journal=article.journal,
+            editor__in=article.articlewrapper.special_issue.editors.all(),
+        )
+    elif directors:
+        parameters = EditorAssignmentParameters.objects.filter(journal=article.journal, editor__in=directors)
+    if parameters:
+        editor = parameters.order_by("workload").first().editor
+        EditorAssignment.objects.create(article=article, editor=editor, editor_type="Editor", notified=True)
+        article.stage = "Assigned"
+        article.save()
+    else:
         print("No editor parameters.")
 
 
 def dispatch_assignment(**kwargs) -> None:
     """Dispatch editors assignment on journal basis, selecting the requested assignment algorithm."""
-    journal = kwargs["article"].journal_id
+    journal = kwargs["article"].journal.code
     if journal in settings.WJS_ARTICLE_ASSIGNMENT_FUNCTIONS:
         import_string(settings.WJS_ARTICLE_ASSIGNMENT_FUNCTIONS.get(journal))(**kwargs)
     else:
