bf642d5ed51672f36ba07b9ee5833affb8af913e
Matteo Gamboz <gamboz@medialab.sissa.it>
2023-01-26 15:16:14 +0100

    oops, did not need to change this fixture, I'm using another one

--- wjs/jcom_profile/tests/conftest.py
+++ wjs/jcom_profile/tests/conftest.py
@@ -251,12 +251,6 @@ def published_articles(admin, editor, article_journal, sections, keywords):
             galley = create_galley(article, file_obj)
             galley.article = article
             galley.last_modified = timezone.now()
-            (language, extension) = file_ext.split(".")
-            label = extension.upper()
-            if language != "":
-                language = language.replace("_", "")
-                label = f"{label} ({language})"
-            galley.label = label
             galley.save()
     return submission_models.Article.objects.all()
 
