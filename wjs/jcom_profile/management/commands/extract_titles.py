f6f316af43eb72f272c651164aa4ac15158cda76
Marco Mizzaro <mmizzaro@medialab.sissa.it>
2023-02-13 14:54:40 +0100

    proof data extraction: id,  pubid, title

--- wjs/jcom_profile/management/commands/extract_titles.py
+++ wjs/jcom_profile/management/commands/extract_titles.py
@@ -3,6 +3,8 @@ from journal import models as journal_models
 from utils.logger import get_logger
 from django.core.management.base import BaseCommand
 from submission.models import Article
+from identifiers import models as identifier_models
+import csv
 
 logger = get_logger(__name__)
 
@@ -13,9 +15,19 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """Command entry point."""
         self.options = options
-        logger.warning(options)
-        logger.warning(Article.objects.all().values("title"))
-        
+        article_id = ''
+        article_id = options["id"]
+        #logger.warning(options)
+        writer = csv.writer(self.stdout, delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerow(['id', 'pubid', 'title'])
+        query_set = Article.objects.all().filter(identifier__id_type='pubid').values("id", "title", "identifier__identifier")
+        if article_id:
+            query_set = Article.objects.all().filter(identifier__id_type='pubid',
+                                                     identifier__identifier=article_id).values("id", "title", "identifier__identifier")
+        for item in query_set:
+          writer.writerow([str(item['id']), str(item['identifier__identifier']), str(item['title'])])
+
+            
     def add_arguments(self, parser):
         """Add arguments to command."""
         parser.add_argument(
