715f307e9f226e814c07e592e25f75c196504655
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2023-02-17 15:08:12 +0100

    Update newsletter tests

--- wjs/jcom_profile/tests/test_newsletters.py
+++ wjs/jcom_profile/tests/test_newsletters.py
@@ -61,7 +61,84 @@ def test_no_newsletters_must_be_sent_when_no_new_articles_with_interesting_keywo
 
 
 @pytest.mark.django_db
-def test_newsletters_are_correctly_sent(
+def test_newsletters_with_news_items_only_must_be_sent(
+    account_factory,
+    recipient_factory,
+    newsletter_factory,
+    news_item_factory,
+    keywords,
+    journal,
+):
+    newsletter = newsletter_factory()
+    news_user, no_news_user = account_factory(email="news@news.it"), account_factory(email="nonews@nonews.it")
+
+    news_recipient = recipient_factory(user=news_user, news=True)
+    news_item_factory(
+        posted=datetime.datetime.now() + datetime.timedelta(days=1),
+    )
+    recipient_factory(user=no_news_user, news=False)
+
+    management.call_command("send_newsletter_notifications")
+
+    assert newsletter.last_sent.date() == datetime.datetime.now().date()
+    assert len(mail.outbox) == 1
+    assert mail.outbox[0].to == [news_recipient.user.email]
+
+
+@pytest.mark.django_db
+def test_newsletters_with_articles_only_must_be_sent(
+    account_factory,
+    recipient_factory,
+    newsletter_factory,
+    article_factory,
+    section_factory,
+    keyword_factory,
+    journal,
+):
+    newsletter = newsletter_factory()
+    correspondence_author = account_factory()
+    newsletter_user_keyword = keyword_factory()
+    newsletter_article_user, no_newsletter_article_user = account_factory(email="article@article.it"), account_factory(
+        email="noarticle@article.it",
+    )
+    newsletter_article = article_factory(
+        journal=journal,
+        date_published=datetime.datetime.now() + datetime.timedelta(days=1),
+        stage="Published",
+        correspondence_author=correspondence_author,
+        section=section_factory(),
+    )
+    newsletter_article.keywords.add(newsletter_user_keyword)
+    newsletter_article.save()
+
+    no_newsletter_article = article_factory(
+        journal=journal,
+        date_published=datetime.datetime.now() + datetime.timedelta(days=1),
+        stage="Published",
+        correspondence_author=correspondence_author,
+        section=section_factory(),
+    )
+    no_newsletter_article.keywords.add(keyword_factory())
+    no_newsletter_article.save()
+
+    newsletter_article_recipient = recipient_factory(
+        user=newsletter_article_user,
+        news=True,
+    )
+    newsletter_article_recipient.topics.add(newsletter_user_keyword)
+    newsletter_article_recipient.save()
+
+    recipient_factory(user=no_newsletter_article_user, news=False)
+
+    management.call_command("send_newsletter_notifications")
+
+    assert newsletter.last_sent.date() == datetime.datetime.now().date()
+    assert len(mail.outbox) == 1
+    assert mail.outbox[0].to == [newsletter_article_recipient.user.email]
+
+
+@pytest.mark.django_db
+def test_newsletters_are_correctly_sent_with_both_news_and_articles(
     account_factory,
     article_factory,
     news_item_factory,
@@ -106,28 +183,3 @@ def test_newsletters_are_correctly_sent(
     filtered_articles = Article.objects.filter(date_published__date__gt=datetime.datetime.now())
     emailed_subscribers = Recipient.objects.filter(topics__in=filtered_articles.values_list("keywords"))
     assert len(mail.outbox) == emailed_subscribers.count()
-
-
-@pytest.mark.django_db
-def test_newsletters_with_news_items(
-    account_factory,
-    recipient_factory,
-    newsletter_factory,
-    keywords,
-    news_item_factory,
-    journal,
-):
-    newsletter = newsletter_factory()
-    news_user, no_news_user = account_factory(email="news@news.it"), account_factory(email="nonews@nonews.it")
-
-    news_recipient = recipient_factory(user=news_user, news=True)
-    news_item_factory(
-        posted=datetime.datetime.now() + datetime.timedelta(days=1),
-    )
-    recipient_factory(user=no_news_user, news=False)
-
-    management.call_command("send_newsletter_notifications")
-
-    assert newsletter.last_sent.date() == datetime.datetime.now().date()
-    assert len(mail.outbox) == 1
-    assert mail.outbox[0].to == [news_recipient.user.email]
