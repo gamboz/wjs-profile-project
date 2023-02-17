f8e831117e92a194d22134724b04d56fb459130c
Lorenzo Pisaneschi <l.pisaneschi@nephila.digital>
2023-02-17 11:35:58 +0100

    Add newsletter news tests

--- wjs/jcom_profile/management/commands/send_newsletter_notifications.py
+++ wjs/jcom_profile/management/commands/send_newsletter_notifications.py
@@ -8,6 +8,7 @@ from core.middleware import GlobalRequestMiddleware
 from django.conf import settings
 from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 from django.template.loader import render_to_string
 from journal.models import Journal
 from submission.models import Article
@@ -64,7 +65,9 @@ class Command(BaseCommand):
             )
         filtered_articles = Article.objects.filter(date_published__date__gt=last_sent)
         filtered_news = NewsItem.objects.filter(posted__date__gt=last_sent)
-        filtered_subscribers = Recipient.objects.filter(topics__in=filtered_articles.values_list("keywords"))
+        filtered_subscribers = Recipient.objects.filter(
+            Q(topics__in=filtered_articles.values_list("keywords")) | Q(news=True),
+        )
         articles_list = list(filtered_articles)
 
         # Templates from themes are found only when there is a request with a journal attached to it.
--- wjs/jcom_profile/tests/test_newsletters.py
+++ wjs/jcom_profile/tests/test_newsletters.py
@@ -13,6 +13,52 @@ def select_random_keywords(keywords):
 
 
 @pytest.mark.django_db
+def test_no_newsletters_must_be_sent_when_no_new_articles_with_interesting_keywords_and_news_exist(
+    account_factory,
+    article_factory,
+    news_item_factory,
+    recipient_factory,
+    section_factory,
+    newsletter_factory,
+    keyword_factory,
+    keywords,
+    journal,
+):
+    newsletter = newsletter_factory()
+    users = []
+    correspondence_author = account_factory()
+    for _ in range(10):
+        users.append(account_factory())
+    for _ in range(10):
+        news_item_factory(
+            posted=datetime.datetime.now() + datetime.timedelta(days=-1),
+        )
+    for user in users:
+        recipient = recipient_factory(user=user)
+        selected_keywords = select_random_keywords(keywords)
+        for keyword in selected_keywords:
+            recipient.topics.add(keyword)
+        recipient.save()
+    for i in range(5):
+        article = article_factory(
+            journal=journal,
+            date_published=datetime.datetime.now() + datetime.timedelta(days=1),
+            stage="Published",
+            correspondence_author=correspondence_author,
+            section=section_factory(),
+        )
+        article.keywords.add(keyword_factory(word=f"{i}-no"))
+        article.keywords.add(keyword_factory(word=f"{i}-interesting"))
+        article.save()
+
+    management.call_command("send_newsletter_notifications")
+
+    newsletter.refresh_from_db()
+    assert newsletter.last_sent.date() == datetime.datetime.now().date()
+    assert len(mail.outbox) == 0
+
+
+@pytest.mark.django_db
 def test_newsletters_are_correctly_sent(
     account_factory,
     article_factory,
@@ -66,12 +112,16 @@ def test_newsletters_with_news_items(
     recipient_factory,
     newsletter_factory,
     keywords,
+    news_item_factory,
     journal,
 ):
     newsletter = newsletter_factory()
     news_user, no_news_user = account_factory(email="news@news.it"), account_factory(email="nonews@nonews.it")
 
     news_recipient = recipient_factory(user=news_user, news=True)
+    news_item_factory(
+        posted=datetime.datetime.now() + datetime.timedelta(days=1),
+    )
     recipient_factory(user=no_news_user, news=False)
 
     management.call_command("send_newsletter_notifications")
