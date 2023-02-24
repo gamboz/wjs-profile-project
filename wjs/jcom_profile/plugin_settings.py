72e7ba0a098b7ee7302a2c2ceb91f7cc1891d6fe
Iacopo Spalletti <i.spalletti@nephila.it>
2023-02-24 12:03:21 +0100

    Update plugin setup

--- wjs/jcom_profile/plugin_settings.py
+++ wjs/jcom_profile/plugin_settings.py
@@ -3,43 +3,46 @@ from django.contrib.contenttypes.models import ContentType
 from journal.models import Journal
 from utils import plugins
 
-PLUGIN_NAME = "WJS Plugin"
-DISPLAY_NAME = "WJS Plugin"
+PLUGIN_NAME = "WJS Home Plugin"
+DISPLAY_NAME = "WJS Home Plugin"
 DESCRIPTION = "A plugin to handle WJS HomePage elements"
 AUTHOR = "Nephila"
 VERSION = "0.1"
+SHORT_NAME = "wjshome"
 JANEWAY_VERSION = "1.3.9"  # the minimum version of Janeway that this plugin works with
 
 
 class WjsPlugin(plugins.Plugin):
+    short_name = SHORT_NAME
     plugin_name = PLUGIN_NAME
     display_name = DISPLAY_NAME
     description = DESCRIPTION
     author = AUTHOR
     version = VERSION
     janeway_version = JANEWAY_VERSION
+    is_workflow_plugin = False
 
 
 def install():
     WjsPlugin.install()
     journal = Journal.objects.first()
     content_type = ContentType.objects.get_for_model(journal)
-    HomepageElement.objects.create(
-        name=PLUGIN_NAME,
+    HomepageElement.objects.get_or_create(
+        name="Latest articles",
         template_path="homepage_elements/latest_articles.html",
         content_type=content_type,
         object_id=journal.pk,
         has_config=False,
     )
-    HomepageElement.objects.create(
-        name=PLUGIN_NAME,
+    HomepageElement.objects.get_or_create(
+        name="Latest news",
         template_path="homepage_elements/news.html",
         content_type=content_type,
         object_id=journal.pk,
         has_config=False,
     )
-    HomepageElement.objects.create(
-        name=PLUGIN_NAME,
+    HomepageElement.objects.get_or_create(
+        name="Publication alert",
         template_path="homepage_elements/newsletter_subscription.html",
         content_type=content_type,
         object_id=journal.pk,
