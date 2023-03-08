ff2496fd6eab26006e5cc87b50759a93be35cacd
Iacopo Spalletti <i.spalletti@nephila.it>
2023-03-08 22:50:16 +0100

    Setup logger

--- wjs/jcom_profile/management/commands/send_newsletter_notifications.py
+++ wjs/jcom_profile/management/commands/send_newsletter_notifications.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand
 from ...newsletter.service import SendNewsletter
 
+import logging
+
+logger = logging.getLogger("wjs.newsletter")
 
 class Command(BaseCommand):
     help = "Send newsletter to enrolled users. This command is intended to be used via a cron task."  # noqa
@@ -11,4 +14,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Run SendNewsletter service to send newsletter"""
-        SendNewsletter().render_and_send(options["journal"], options["force"])
+        messages = SendNewsletter().render_and_send(options["journal"], options["force"])
+        for message in messages:
+            logger.debug(message)
