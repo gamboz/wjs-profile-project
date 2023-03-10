ec98346e0f258cbaa92baba68c5de73293d98932
Iacopo Spalletti <i.spalletti@nephila.it>
2023-03-10 16:46:54 +0100

    add docs

--- wjs/jcom_profile/tests/conftest.py
+++ wjs/jcom_profile/tests/conftest.py
@@ -78,6 +78,7 @@ def clear_cache():
 
 @pytest.fixture
 def mock_premailer_load_url(mocker):
+    """Provide a empty response for css when fetched by premailer."""
     mock = mocker.patch(
         "premailer.premailer.Premailer._load_external_url",
         return_value=""
