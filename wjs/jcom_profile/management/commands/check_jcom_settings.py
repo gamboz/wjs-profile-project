eda9258da9dff0e565f54c2c7c0a9b8d9fff6bcf
Matteo Gamboz <gamboz@medialab.sissa.it>
2023-03-17 14:21:31 +0100

    [OT] tidy up

--- wjs/jcom_profile/management/commands/check_jcom_settings.py
+++ wjs/jcom_profile/management/commands/check_jcom_settings.py
@@ -227,12 +227,7 @@ class Command(BaseCommand):
             #
             # Email message that is sent when an anonymous user
             # subscribes to newsletters. - Message email body
-            # (
-            #     "email",
-            #     "subscribe_custom_email_message",
-            #     "Hi,\nYou requested to subscribe to {} journal newsletters.\n"
-            #     "To continue click the following link:{}",
-            # ),
+            # ("email", "subscribe_custom_email_message", ...
             # Managed by wjs/jcom_profile/management/commands/add_publication_alert_settings.py
             #
             # Janeway Support Contact for Staff - Support message to
