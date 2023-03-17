397a70032d2406509b6d5d266ae3b343c527f156
Matteo Gamboz <gamboz@medialab.sissa.it>
2023-03-17 11:33:34 +0100

    fix citation_pdf_url

--- wjs/jcom_profile/urls.py
+++ wjs/jcom_profile/urls.py
@@ -152,9 +152,9 @@ urlpatterns = [
     # same subfolder as the paper's landing page (see #107);
     # citation_pdf_url have the form article/pubid/JCOM123/galley.id
     url(
-        r"article/pubid/(?P<pubid>[\w.()-]+)/(?P<galley_id>\d+)",
-        views.JcomFileRedirect.as_view(),
-        name="jcom_redirect_file",
+        r"^article/(?P<identifier_type>pubid)/(?P<identifier>.+)/download/pdf/$",
+        journal_views.serve_article_pdf,
+        name="serve_article_pdf",
     ),
     # Redirects - end
 ]
