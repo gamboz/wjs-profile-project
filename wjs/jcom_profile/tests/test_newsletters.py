7c78d1cc2a3db2ca53277b5cd8cc57f061de6b81
Mark Caglienzi <m.caglienzi@nephila.digital>
2023-03-29 13:45:27 +0200

    Fix test

--- wjs/jcom_profile/tests/test_newsletters.py
+++ wjs/jcom_profile/tests/test_newsletters.py
@@ -515,7 +515,6 @@ def test_registration_as_logged_user_when_a_recipient_does_not_exist(
     assert new_recipients.count() == 1
     # Check the new Recipient's characteristics
     new_recipient = new_recipients.first()
-    assert new_recipient.email == jcom_user.email
     assert new_recipient.newsletter_token == ""
     # Check the redirect
     last_url, status_code = response.redirect_chain[-1]
